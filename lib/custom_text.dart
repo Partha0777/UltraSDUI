d49ee679d296faa556250385920f2370ea572eaa
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-03-31 11:17:55 +0530

    Fixed dart auto suggestions issue.

--- lib/custom_text.dart
+++ lib/custom_text.dart
@@ -1,5 +1,12 @@
 
 
+import 'package:flutter/widgets.dart';
+
 class CustomText extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
 
 }
